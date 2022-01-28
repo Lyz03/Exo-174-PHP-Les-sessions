3b9f0e9f8aa73f1be7afacc78b35680d85d6d361
Lyz03 <lizoe@netcourrier.com>
2022-01-28 12:04:11 +0100

    exo 174

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
 
 // Premiere ligne
+session_start();
+$_SESSION['couleur'] = 'rouge';
 
 
+require __DIR__ . '/index.html';
 
+if (isset($_POST['submit'])) {
+    $_SESSION['couleur'] = $_POST['color'];
+}
 
-
-
-
-
+echo $_SESSION['couleur'];
